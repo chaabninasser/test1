6f79b57b964da7546c01f315dc6dbe2787cfee38
chaabninasser <40791395+chaabninasser@users.noreply.github.com>
2018-10-11 18:39:49 +0200

    first-commit

---
+++ test1/exercice2.js
@@ -0,0 +1,29 @@
+function replace(a) {
+    var a;
+    var j;
+    var k;
+    var h = 0;
+
+    a = 1 || a = 0;
+    T = [a, a, a, a, a, a, a, a, a];
+
+    for (var i = 0; i <= T.length; i++) {
+        if (T[i] = 0) {
+            i = j;
+            T[i] = 1;
+            if (T[i] = T[i + 1]) {
+                i++
+            }
+            else {
+                i = k
+            }
+            h = k - j + 1;
+        }
+        if (h <= k - j + 1) {
+            h = k - j + 1;
+        }
+
+    }
+    return h
+
+}
